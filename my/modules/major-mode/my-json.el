dee9e0137f1d913c9c1d7898224f387722d56335
liyunteng <liyunteng@streamocean.com>
2017-08-02 14:41:30 +0800

    add my-json

---
+++ my/modules/major-mode/my-json.el
@@ -0,0 +1,29 @@
+;;; my-json.el --- json mode                         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  liyunteng
+
+;; Author: liyunteng <li_yunteng@163.com>
+;; Keywords: lisp, abbrev
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(my-require-package 'json-mode)
+
+(provide 'my-json)
+;;; my-json.el ends here
