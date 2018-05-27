6cd2c0ce0f590503d786bcf2e61e174743695378
liyunteng <li_yunteng@163.com>
2018-05-27 18:56:00 +0800

    add font config

--- my/modules/my-gui.el
+++ my/modules/my-gui.el
@@ -26,10 +26,14 @@
 
 ;; 使用系统字体
 (setq-default font-use-system-font t)
-;; (if (eq system-type 'linux)
-;;     (custom-set-faces
-;;      `(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant
-;;      normal :weight normal :height 113 :width normal))))))
+;; 中文使用wqy-microhei,其他使用DejaVu Sans Mono
+(create-fontset-from-fontset-spec
+ "-*-DejaVu Sans Mono-normal-normal-normal-*-15-*-*-*-m-0-fontset-my,
+chinese-gbk: -*-WenQuanYi Micro Hei-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1,
+chinese-iso-8bit: -*-WenQuanYi Micro Hei-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1,
+chinese-big5: -*-WenQuanYi Micro Hei-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1")
+(setq default-frame-alist (append '((font . "fontset-my")) default-frame-alist))
+(set-frame-font "fontset-my")
 
 (use-package smart-mode-line
   :ensure t
@@ -106,97 +110,6 @@ Otherwise,add it to a queue of actions to perform after the first graphical fram
 (let ((no-border '(internal-border-width . 0)))
   (add-to-list 'default-frame-alist no-border)
   (add-to-list 'initial-frame-alist no-border))
-
-;; font
-;; (defvar my-default-font '("DejaVu Sans"
-;;                           :size 16
-;;                           :weight normal
-;;                           :width normal
-;;                           :powerline-scale 1.1))
-(defvar my-default-font nil)
-
-(defun my-set-default-font (plists)
-  "Set the font given the passed PLISTS.
-
-PLISTS has either the form (\"fontname\" :prop1 val1 :prop2 val2 ...)
-or is a list of such. The first font that can be found will be used.
-
-The return value is nil if no font was found, truthy otherwise."
-  (unless (listp (car plists))
-    (setq plists (list plists)))
-  (catch 'break
-    (dolist (plist plists)
-      (when (find-font (font-spec :name (car plist)))
-        (let* ((font (car plist))
-               (props (cdr plist))
-               (scale (plist-get props :powerline-scale))
-               (font-props (my-mplist-remove
-                            (my-mplist-remove props :powerline-scale)
-                            :powerline-offset))
-               (fontspec (apply 'font-spec :name font font-props)))
-          (my/echo "Setting font \"%s\"..." font)
-          (set-frame-font fontspec nil t)
-          (push `(font . ,(frame-parameter nil 'font)) default-frame-alist)
-          (setq-default powerline-scale scale)
-          (setq-default powerline-height (my--compute-powerline-height))
-          ;; fallback font for unicode characters used in spacemacs
-          (pcase system-type
-            (`gnu/linux
-             (setq fallback-font-name "NanumGothic")
-             (setq fallback-font-name2 "NanumGothic"))
-            (`darwin
-             (setq fallback-font-name "Arial Unicode MS")
-             (setq fallback-font-name2 "Arial Unicode MS"))
-            (`windows-nt
-             (setq fallback-font-name "MS Gothic")
-             (setq fallback-font-name2 "Lucida Sans Unicode"))
-            (`cygwin
-             (setq fallback-font-name "MS Gothic")
-             (setq fallback-font-name2 "Lucida Sans Unicode"))
-            (other
-             (setq fallback-font-name nil)
-             (setq fallback-font-name2 nil)))
-          (when (and fallback-font-name fallback-font-name2)
-            ;; remove any size or height properties in order to be able to
-            ;; scale the fallback fonts with the default one (for zoom-in/out
-            ;; for instance)
-            (let* ((fallback-props (my-mplist-remove
-                                    (my-mplist-remove font-props :size)
-                                    :height))
-                   (fallback-spec (apply 'font-spec
-                                         :name fallback-font-name
-                                         fallback-props))
-                   (fallback-spec2 (apply 'font-spec
-                                          :name fallback-font-name2
-                                          fallback-props)))
-              ;; window numbers
-              (set-fontset-font "fontset-default"
-                                '(#x2776 . #x2793) fallback-spec nil 'prepend)
-              ;; mode-line circled letters
-              (set-fontset-font "fontset-default"
-                                '(#x24b6 . #x24fe) fallback-spec nil 'prepend)
-              ;; mode-line additional characters
-              (set-fontset-font "fontset-default"
-                                '(#x2295 . #x22a1) fallback-spec nil 'prepend)
-              ;; new version lighter
-              (set-fontset-font "fontset-default"
-                                '(#x2190 . #x2200) fallback-spec2 nil 'prepend))))
-        (throw 'break t)))
-    nil))
-
-(defun my--compute-powerline-height ()
-  "Return an adjusted powerline height."
-  (let ((scale (if (and (boundp 'powerline-scale) powerline-scale)
-                   powerline-scale 1)))
-    (truncate (* scale (frame-char-height)))))
-
-(when my-default-font
-  (unless (my-set-default-font my-default-font)
-    (message
-     "Cannot find any of the specified fonts (%s)! Font settings may not be correct."
-     (if (listp (car my-default-font))
-         (mapconcat 'car my-default-font ", ")
-       (car my-default-font)))))
 
 
 (defun my-fix-up-xterm-control-arrows ()
